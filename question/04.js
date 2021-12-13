2c178a8994f51558bfd6d45d228c1b6aab14d2e3
GiveMe-A-Name <a455300764b@hotmail.com>
2021-12-13 08:48:37 +0800

    add

---
+++ question/04.js
@@ -0,0 +1,8 @@
+// 京东前端一面
+// 给定两个字符串 strA 和 strB, 求 strB 在 strA 中出现的次数
+// Eample:
+// 1. strA = "aaaa" strB = "aa" 其中strB在strA中出现过两次, 输出2
+// 2. strA = "abcabd" strB="abd" 其中strB在strA中出现过两次，输出2
+// 3. strA = "abwexyijxyyxy" strB="xy" 其中strB在strA中出现过三次，输出3
+
+function CounterStr(strA, strB) {}
